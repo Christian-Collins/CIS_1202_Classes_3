e5057760c86773ec09a6e0a95f375ce2f683b822
Christian Collins <christian.collins1@sinclair.edu>
2024-04-20 17:36:33 -0400

    Added empty Vehicle class, and empty Car and Truck derived classes. Also added empty "int main" function to source.cpp as well as "#include" for iostream and the three classes. Finally, added comments for my name, assignment name, and date

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+// Christian Collins
+// CIS 1202 501
+// April 20, 2024
+
+#include <iostream>
+#include "Vehicle.h"
+#include "Car.h"
+#include "Truck.h"
+
+using namespace std;
+int main()
+{
+	
+}
+
